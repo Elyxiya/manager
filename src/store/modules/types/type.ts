82705d62658ccc1e5c41d89e5f0d9d46127927f3
Elyxiya <3013693518@qq.com>
2025-03-24 04:58:50 +0800

    feat: login组件功能

--- src/store/modules/types/type.ts
+++ src/store/modules/types/type.ts
@@ -1,12 +1,12 @@
-import type { RouteRecordRaw } from "vue-router";
+import type { RouteRecordRaw } from 'vue-router'
 // import type { CategoryObj } from "@/api/product/attr/type";
 //定义小仓库数据state类型
 export interface UserState {
-    token: string | null;
-    menuRoutes: RouteRecordRaw[],
-    username: string,
-    avatar: string,
-    buttons:string[]
+  token: string | null
+  menuRoutes: RouteRecordRaw[]
+  username: string
+  avatar: string
+  buttons: string[]
 }
 
 //定义分类仓库state对象的ts类型
